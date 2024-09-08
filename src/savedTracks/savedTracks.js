9ae02e1e335bc7f2f372ef2e4876e8f10559eb95
Baon2024 <Baon2020@protonmail.com>
2024-09-08 09:28:56 +0100

    first add of savedTracks

---
+++ src/savedTracks/savedTracks.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+
+
+
+function SavedTracks({getSavedSongs, responseState}) {
+const [ savedSongs, setSavedSongs ] = useState([]);
+
+
+
+
+//have the actual function to get saved tracks called using useEffect on page load? and only change is savedSongs changed (so in displacement array)
+// [savedSongs]. but write function in app.js page, then pass down to this child.
+
+useEffect(() => {
+   
+  async function fetchSavedSongs() {
+    try {
+        const songs = await getSavedSongs(); // Await the result of the async function
+        console.log('Fetched Songs:', songs);
+        setSavedSongs(songs);
+      } catch (error) {
+        console.error('Error fetching saved songs:', error);
+      }
+    
+}
+  fetchSavedSongs();
+  console.log('Your saved playlist is: ' + savedSongs);
+}, [responseState])
+
+
+/*{savedSongs.map((savedPlaylist) => {
+    <div key={savedPlaylist.id}>
+    <p>{savedPlaylist.name}</p>
+  </div>
+ })}*/
+
+    return (
+      <div>
+        <p>Existing playlists</p>
+        {Array.isArray(savedSongs) && savedSongs.map((savedPlaylist) => (
+          <div key={savedPlaylist.id}>
+          <p>{savedPlaylist.name}</p>
+          </div>
+        ))}
+      </div>
+    )
+}
+export default SavedTracks;
